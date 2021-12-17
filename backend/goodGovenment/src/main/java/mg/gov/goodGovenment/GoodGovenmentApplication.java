c072891a23e84a7bf6c44ebd1c728e63ba873748
Mandresy Rabenja <mandresyrabenj@gmail.com>
2021-12-17 11:17:25 +0300

    Ajout projet spring

---
+++ backend/goodGovenment/src/main/java/mg/gov/goodGovenment/GoodGovenmentApplication.java
@@ -0,0 +1,13 @@
+package mg.gov.goodGovenment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodGovenmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodGovenmentApplication.class, args);
+	}
+
+}
